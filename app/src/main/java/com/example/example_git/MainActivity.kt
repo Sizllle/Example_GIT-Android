2bb927e2154260596d4726dbfaf5df309c54c604
Sizllle <vladislaw.kuzminov2017@gmail.com>
2023-05-30 22:22:00 +0300

    Connect viewBinding in build.gradle. Create button in activity_main.xml and write function for change text on button click MainActivity.kt

--- app/src/main/java/com/example/example_git/MainActivity.kt
+++ app/src/main/java/com/example/example_git/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.example_git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.example_git.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(Sizlle: Bundle?) {
+        super.onCreate(Sizlle)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun onClickButtonChange (view: View){
+        binding.tvMain.text = "We change text"
     }
 }
